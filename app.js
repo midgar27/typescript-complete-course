4af9c275b1d548199798db8d4e32e3c7428b86d6
midgar27 <rgodwin81@gmail.com>
2019-11-16 13:13:14 +0000

    End of Section 11

--- app.js
+++ app.js
@@ -2,4 +2,12 @@
 // string
 var myName = 'Rob';
 // myName = 28;
+// number
+var myAge = 27;
+// myAge = 'Rob';
+// boolean
+var hasHobbies = false;
+// hasHobbies = 1;
 console.log('myName = ' + myName);
+console.log('myAge = ' + myAge);
+console.log('hasHobbies = ' + hasHobbies);
--- app.ts
+++ app.ts
@@ -2,4 +2,14 @@
 let myName = 'Rob';
 // myName = 28;
 
+// number
+let myAge = 27;
+// myAge = 'Rob';
+
+// boolean
+let hasHobbies = false;
+// hasHobbies = 1;
+
 console.log('myName = ' + myName);
+console.log('myAge = ' + myAge);
+console.log('hasHobbies = ' + hasHobbies);
